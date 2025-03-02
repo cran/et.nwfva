9ecf7a8f2c0d2b9eef9fe8f85cc15e759b3b37e5
Robert Nuske <robert.nuske@mailbox.org>
2025-03-02 22:10:01 +0000

    version 0.2.0

--- R/hoehen.R
+++ R/hoehen.R
@@ -16,14 +16,19 @@
 #' Berechnung erfolgt entweder über einen funktionalisierten Bonitätsfächer oder
 #' über Inter-/Extrapolation der Ertragstafeln mittels Dreisatz.
 #'
-#' Die Bestimmung der Bestandeshöhe über die funktionalisierten Bonitätsfächer
-#' basiert auf nichtlinearen Modellen, die durch Anpassung an die Ober- und
-#' Mittelhöhen über dem Alter bei mäßiger Durchforstung bzw. für
-#' Eichen-Oberhöhen bei starker Durchforstung aus der Ertragstafelsammlung von
-#' Schober (1995) entstanden. Für die Kiefer wurde hierbei die
-#' Chapman-Richards-Funktion (Richards 1959) und für alle anderen Baumarten die
-#' Wachstumsfunktion von Sloboda (1971) genutzt. Die Bestandeshöhen der neuen
-#' Ertragstafeln (Albert et al. 2021) sind die tabellarisierten Werte eben
+#' Die Ermittlung der Bestandesoberhöhe mithilfe der funktionalisierten
+#' Bonitätsfächer basiert auf nichtlinearen Modellen, die durch Anpassung an die
+#' alters- und bonitätsabhängigen Oberhöhenverläufe bei mäßiger bzw. für Eiche
+#' bei starker Durchforstung gemäß der Ertragstafelsammlung von Schober (1995)
+#' entwickelt wurden. Für die Kiefer wurde hierbei die Chapman-Richards-Funktion
+#' (Richards 1959) und für alle anderen Baumarten die Wachstumsfunktion von
+#' Sloboda (1971) genutzt, jeweils in der algebraischen Differenzenform. Die
+#' Bonitätsfächermodelle der Bestandesmittelhöhen ergaben sich durch
+#' Modellierung der Relationen der Mittel- und Oberhöhen in den den neuen
+#' Ertragstafel zugrundeliegenden Simulationsbeständen (s. Vignette "Hinweise
+#' zum Geleit") und funktionalen Ausgleich der resultierenden Mittelhöhen mit
+#' den oben genannten Funktionstypen. Die Bestandesober- und -mittelhöhen der
+#' neuen Ertragstafeln (Albert et al. 2021) sind die tabellarisierten Werte eben
 #' dieser funktionalisierten Bonitätsfächer.
 #'
 #' Der entscheidende Vorteil der Bonitätsfächermodelle gegenüber dem klassischen
@@ -48,7 +53,7 @@
 #' @param art Baumartenbezeichnung entweder als Kürzel, deutscher Name,
 #'   lateinischer Name oder in niedersächsischer Kodierung.
 #'   Für vorhandene Arten siehe [et_liste()].
-#' @param alter Bestandesalter in Jahrenals ganze Zahl. Bei Methode `klassisch`
+#' @param alter Bestandesalter in Jahren als ganze Zahl. Bei Methode `klassisch`
 #'   zwischen 5 und max. zulässigem Alter (Ei 220, Bu 180 und Fi, Dgl, Ki 160).
 #' @param bon Bonität als Zahl. Zulässig sind relative Ertragsklassen im
 #'   Interval \[-2,4\] bzw. \[-3,7\] bei Methode `"klassisch"` bzw.
@@ -68,10 +73,11 @@
 #' @param ... Weitere Parameter, wie z.B. für funkt. Bonitätsfächermodell auf
 #'   Basis der Hossfeld-Funktion (s. Details).
 #'
-#' @return Numerischer Vektor mit Bestandeshöhen in Meter. Für Werte außerhalb
-#'   des zulässigen Alters- und Bonitätsintervalls wird `NA` ausgegeben.
-#'   Die klassische Methode kann für sehr junge Alter und schlechte Bonitäten
-#'   zu negativen Bestandeshöhen führen, dann wird ebenfalls `NA` ausgegeben.
+#' @return Numerischer Vektor mit Bestandeshöhen in Meter gerundet auf eine
+#'   Nachkommastelle. Für Werte außerhalb des zulässigen Alters- und
+#'   Bonitätsintervalls wird `NA` ausgegeben. Die klassische Methode kann für
+#'   sehr junge Alter und schlechte Bonitäten zu negativen Bestandeshöhen
+#'   führen, dann wird ebenfalls `NA` ausgegeben.
 #'
 #' @author Robert Nuske (klassisch), Kai Staupendahl (funktional)
 #'
@@ -118,9 +124,18 @@ et_hoehe <- function(art, alter, bon, bon_typ="relativ",
 
   if(missing(art) | missing(alter) | missing(bon))
     stop("art, alter und bon m\u00fcssen angegeben werden.")
-  if(!is.wholenumber(alter))
+  l <- c(length(art), length(alter), length(bon))
+  if(any(l == 0))
+    stop("art, alter und bon k\u00f6nnen nicht NULL sein")
+  if(length(unique(l[l != 1])) > 1)
+    stop("Die L\u00e4nge von art, alter und bon muss gleich oder eins sein")
+  d <- data.frame(art, alter, bon)
+  h <- rep(NA, nrow(d))
+  na <- is.na(d$art) | is.na(d$alter) | is.na(d$bon)
+  d <- d[!na, ]
+  if(!is.wholenumber(d$alter))
     stop("alter muss ganzzahlig sein.")
-  if(!is.numeric(bon))
+  if(!is.numeric(d$bon))
     stop("bon muss numerisch sein.")
   bon_typ <- match.arg(bon_typ, c("relativ", "absolut"))
   hoehe_typ <- match.arg(hoehe_typ, c("mittel", "ober"))
@@ -129,8 +144,8 @@ et_hoehe <- function(art, alter, bon, bon_typ="relativ",
   if(methode == "funktional"){
 
     if(hoehe_typ == "mittel"){
-      h <- funk_hg(art, alter, bon,
-                   bon_als_ekl=isTRUE(bon_typ == "relativ"))
+      h[!na] <- funk_hg(d$art, d$alter, d$bon,
+                        bon_als_ekl=isTRUE(bon_typ == "relativ"))
     } else { # ober
 
       # behandle dot-dot-dot auf der Suche nach hoss
@@ -141,11 +156,11 @@ et_hoehe <- function(art, alter, bon, bon_typ="relativ",
         } else { stop("Unbekannte Parameter in ... \u00fcbergeben.") }
       } else { hoss <- FALSE }
 
-      h <- funk_h100(art, alter, bon,
-                     bon_als_ekl=isTRUE(bon_typ == "relativ"), hoss)
+      h[!na] <- funk_h100(d$art, d$alter, d$bon,
+                          bon_als_ekl=isTRUE(bon_typ == "relativ"), hoss)
     }
   } else { # klassisch
-    h <- klas_hoehe(art, alter, bon, bon_typ, hoehe_typ)
+    h[!na] <- klas_hoehe(d$art, d$alter, d$bon, bon_typ, hoehe_typ)
   }
   return(round(h, 1))
 }
